1dcdd418ced406b53422ca281debae799b22b692
Alexander Bückig <AlexBueckig@gmail.com>
2016-11-03 13:26:04 +0100

    clientside validation. extract TextFieldGroup

--- client/src/components/signup/SignupForm.js
+++ client/src/components/signup/SignupForm.js
@@ -3,6 +3,38 @@
  */
 
 import React, { Component, PropTypes } from 'react';
+import Validator from 'validator';
+import isEmpty from 'lodash/isEmpty';
+
+import TextFieldGroup from '../common/TextFieldGroup';
+
+function validateInput(data) {
+    let errors = {};
+
+    if(!Validator.isEmail(data.email)) {
+        errors.email = 'Email is invalid';
+    }
+    if(Validator.isEmpty(data.email)) {
+        errors.email = 'This field is required';
+    }
+    if(Validator.isEmpty(data.username)) {
+        errors.username = 'This field is required';
+    }
+    if(Validator.isEmpty(data.password)) {
+        errors.password = 'This field is required';
+    }
+    if(!Validator.equals(data.password, data.passwordConfirmation)) {
+        errors.passwordConfirmation = 'Passwords must match'
+    }
+    if(Validator.isEmpty(data.passwordConfirmation)) {
+        errors.passwordConfirmation = 'This field is required';
+    }
+
+    return {
+        errors,
+        isValid: isEmpty(errors)
+    }
+}
 
 class SignupForm extends Component {
     constructor(props) {
@@ -11,7 +43,9 @@ class SignupForm extends Component {
             username: '',
             email: '',
             password: '',
-            passwordConfirmation: ''
+            passwordConfirmation: '',
+            errors: {},
+            isLoading: false
         };
 
         this.onChange = this.onChange.bind(this);
@@ -22,57 +56,64 @@ class SignupForm extends Component {
         this.setState({[e.target.name]: e.target.value})
     }
 
+    isValid() {
+        const { errors, isValid } = validateInput(this.state);
+
+        if(!isValid) {
+            this.setState({errors});
+        }
+
+        return isValid;
+    }
+
     onSubmit(e) {
         e.preventDefault();
-        this.props.userSignupRequest(this.state);
+        if(this.isValid()) {
+            this.setState({errors: {}, isLoading: true});
+            this.props.userSignupRequest(this.state).then(
+                () => {},
+                (err) => this.setState({errors: err.response.data, isLoading: false})
+            );
+        }
     }
 
     render() {
+        const { errors } = this.state;
         return (
             <form onSubmit={this.onSubmit}>
                 <h1>Join our community!</h1>
+                <TextFieldGroup
+                    field="username"
+                    value={this.state.username}
+                    label="Username"
+                    onChange={this.onChange}
+                    error={errors.username}
+                />
+                <TextFieldGroup
+                    field="email"
+                    value={this.state.email}
+                    label="Email"
+                    onChange={this.onChange}
+                    error={errors.email}
+                />
+                <TextFieldGroup
+                    field="password"
+                    value={this.state.password}
+                    label="Password"
+                    type="password"
+                    onChange={this.onChange}
+                    error={errors.password}
+                />
+                <TextFieldGroup
+                    field="passwordConfirmation"
+                    value={this.state.passwordConfirmation}
+                    label="Repeat Password"
+                    type="password"
+                    onChange={this.onChange}
+                    error={errors.passwordConfirmation}
+                />
                 <div className="form-group">
-                    <label className="control-label">Username</label>
-                    <input
-                        value={this.state.username}
-                        onChange={this.onChange}
-                        type="text"
-                        name="username"
-                        className="form-control"
-                    />
-                </div>
-                <div className="form-group">
-                    <label className="control-label">Email</label>
-                    <input
-                        value={this.state.email}
-                        onChange={this.onChange}
-                        type="text"
-                        name="email"
-                        className="form-control"
-                    />
-                </div>
-                <div className="form-group">
-                    <label className="control-label">Password</label>
-                    <input
-                        value={this.state.password}
-                        onChange={this.onChange}
-                        type="password"
-                        name="password"
-                        className="form-control"
-                    />
-                </div>
-                <div className="form-group">
-                    <label className="control-label">Repeat Password</label>
-                    <input
-                        value={this.state.passwordConfirmation}
-                        onChange={this.onChange}
-                        type="password"
-                        name="passwordConfirmation"
-                        className="form-control"
-                    />
-                </div>
-                <div className="form-group">
-                    <button className="btn btn-primary btn-lg">Sign up</button>
+                    <button className="btn btn-primary btn-lg" disabled={this.state.isLoading}>Sign up</button>
                 </div>
             </form>
         );
