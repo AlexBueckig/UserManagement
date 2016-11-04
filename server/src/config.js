207f036b76ac81cfd4d49e74ca7ba0df6e34c024
Alexander Bückig <AlexBueckig@gmail.com>
2016-11-04 14:28:25 +0100

    verify user credentials

---
+++ server/src/config.js
@@ -0,0 +1,7 @@
+/**
+ * Created by Alex on 04.11.2016.
+ */
+
+export default {
+  jwtSecret: 'test123'
+};
--- server/src/index.js
+++ server/src/index.js
@@ -7,6 +7,7 @@ import bodyParser from 'body-parser';
 import path from 'path';
 
 import users from './routes/users';
+import auth from './routes/auth';
 
 let app = express();
 
@@ -14,6 +15,7 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
 
 app.use('/api/users', users);
+app.use('/api/auth', auth);
 
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname + '/index.html'));
