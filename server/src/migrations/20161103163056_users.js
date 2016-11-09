866c0ba0ab2a76eab0a3b9c796154dbb45273950
Alexander Bückig <AlexBueckig@gmail.com>
2016-11-09 12:25:06 +0100

    Semikolon hinzugefügt

--- server/src/migrations/20161103163056_users.js
+++ server/src/migrations/20161103163056_users.js
@@ -1,14 +1,14 @@
-exports.up = function (knex, Promise) {
+exports.up = function (knex) {
     return knex.schema.createTable('users', table => {
             table.increments();
             table.string('username').notNullable().unique();
             table.string('email').notNullable().unique();
-            table.string('password_digest').notNullable()
+            table.string('password_digest').notNullable();
             table.timestamps();
         }
     );
 };
 
-exports.down = function (knex, Promise) {
+exports.down = function (knex) {
     return knex.schema.dropTable('users');
 };
