0c2fa057d9f96cbfe3fb6e2c4d5b2bdbb0eb0685
Alexander Bückig <AlexBueckig@gmail.com>
2016-11-10 13:59:00 +0100

    Projekt Komponenten hinzugefügt

---
+++ client/src/components/projects/ProjectsPage.js
@@ -0,0 +1,14 @@
+/**
+ * Created by Alex on 09.11.2016.
+ */
+import React, { Component } from 'react';
+
+class ProjectsPage extends Component {
+    render() {
+        return (
+            <div>ProjectsPage</div>
+        );
+    }
+}
+
+export default ProjectsPage;
