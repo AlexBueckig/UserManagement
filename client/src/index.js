7ad37159af50680171fabb7974d4b2433efc48da
Alexander Bückig <AlexBueckig@gmail.com>
2016-11-10 14:02:32 +0100

    store in eigene Datei ausgelagert

--- client/src/index.js
+++ client/src/index.js
@@ -1,33 +1,27 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './styles/index.css';
-
-import { createStore, applyMiddleware, compose } from 'redux';
-import { Provider } from 'react-redux'
-import thunk from 'redux-thunk'
+import {Provider} from 'react-redux'
+import { BrowserRouter } from 'react-router';
 import jwt_decode from 'jwt-decode';
 
+import './styles/index.css';
+import '../node_modules/bootstrap/dist/css/bootstrap.css';
+
 import App from './components/App';
-import rootReducer from './rootReducer';
 import setAuthorizationToken from './utils/setAuthorizationToken';
-import { setCurrentUser } from './actions/authActions';
+import {setCurrentUser} from './actions/authActions';
+import store from './store';
 
-const store = createStore(
-    rootReducer,
-    compose(
-        applyMiddleware(thunk),
-        window.devToolsExtension ? window.devToolsExtension() : f => f
-    )
-);
-
-if(localStorage.jwtToken) {
+if (localStorage.jwtToken) {
     setAuthorizationToken(localStorage.jwtToken);
     store.dispatch(setCurrentUser(jwt_decode(localStorage.jwtToken)));
 }
 
 ReactDOM.render(
     <Provider store={store}>
-        <App />
+        <BrowserRouter>
+            <App />
+        </BrowserRouter>
     </Provider>,
     document.getElementById('root')
 );
