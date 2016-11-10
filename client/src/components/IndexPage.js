5b3e83ef7a3fe80d6453915294c3e453ac984999
Alexander Bückig <AlexBueckig@gmail.com>
2016-11-10 14:00:29 +0100

    IndexPage hinzugefügt

---
+++ client/src/components/IndexPage.js
@@ -0,0 +1,12 @@
+/**
+ * Created by Alex on 09.11.2016.
+ */
+import React from 'react';
+
+const IndexPage = (props) => {
+    return (
+        <div>Home!!!</div>
+    );
+};
+
+export default IndexPage;
